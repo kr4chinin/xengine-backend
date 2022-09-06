bb20fc75417f06329f41699162ee32926111c4b6
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-06 13:39:15 +0300

    feat: added ErrorHandler middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import fileUpload from 'express-fileupload'
 import { config } from 'dotenv'
 import models from './models/index.js'
 import sequelize from './db.js'
+import ErrorHandler from './middleware/ErrorHandler.js'
 
 config()
 
@@ -14,6 +15,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))
+
+app.use(ErrorHandler)
 
 app.get('/', (req, res) => {
 	res.status(200).json({
