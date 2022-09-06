d6b42b47dd60882f50321dc150fdcd3b30e3cfff
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-06 21:01:05 +0300

    feat: now server understands static files

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { config } from 'dotenv'
 import models from './models/index.js'
 import sequelize from './db.js'
 import apiRouter from './routes/index.js'
+import { __dirname } from './helpers/dirname.js'
+import path from 'path'
 
 config()
 
@@ -16,24 +18,27 @@ app.use(cors())
 app.use(express.json())
 app.use(fileUpload({}))
 
+// To make server understand static files
+app.use(express.static(path.resolve(__dirname, '..', 'static')))
+
 app.use('/api', apiRouter)
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
 	res.status(200).json({
 		message: 'Server is working!'
 	})
 })
 
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}...`)
-        })
-    } catch (e) {
-        console.log(e)
-    }
+	try {
+		await sequelize.authenticate()
+		await sequelize.sync()
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}...`)
+		})
+	} catch (e) {
+		console.log(e)
+	}
 }
 
 start()
