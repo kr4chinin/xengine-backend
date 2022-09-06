14ea9eb7eea1d22688293b3a5e9450442458714b
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-06 12:10:51 +0300

    feat: server initial setup

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import fileUpload from 'express-fileupload'
+import { config } from 'dotenv'
+
+config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(fileUpload({}))
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		message: 'Server is working!'
+	})
+})
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}...`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
