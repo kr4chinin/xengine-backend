49c0287f9bcf04a4dd91330ddf30182e8b505025
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-06 13:40:31 +0300

    fix: fixed import path

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -1,13 +1,13 @@
-import { ApiError } from "../helpers/apiError";
+import { ApiError } from '../helpers/apiError.js'
 
 export default function (err, req, res, next) {
-    // If we got an error from the controller we will send it to the client
-    // with underlying message and status code
+	// If we got an error from the controller we will send it to the client
+	// with underlying message and status code
 
 	if (err instanceof ApiError) {
 		return res.status(err.status).json({ message: err.message })
 	}
 
-    // Else we can assume that this is an unknown internal server error
+	// Else we can assume that this is an unknown internal server error
 	return res.status(500).json({ message: 'Unknown internal server error' })
 }
