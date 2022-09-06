cf7db9831b00161345186b783b15a73b28f0d58f
Ilya Kruchinin <kr4chinin@mail.ru>
2022-09-06 13:25:06 +0300

    feat: helper class for handling errors in the API

---
+++ helpers/apiError.js
@@ -0,0 +1,27 @@
+export class ApiError extends Error {
+	constructor(status, message) {
+		super()
+		this.status = status
+		this.message = message
+	}
+
+	static badRequest(message) {
+		return new ApiError(400, message)
+	}
+
+	static unauthorized(message) {
+		return new ApiError(401, message)
+	}
+
+	static forbidden(message) {
+		return new ApiError(403, message)
+	}
+
+	static notFound(message) {
+		return new ApiError(404, message)
+	}
+
+	static internal(message) {
+		return new ApiError(500, message)
+	}
+}
